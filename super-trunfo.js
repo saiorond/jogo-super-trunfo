7bfdabc23b42e1f99ee6210a6148d7704430273d
Débora <d.saioron@hotmail.com>
2022-07-30 00:06:56 -0300

    iniciando o js

---
+++ super-trunfo.js
@@ -0,0 +1,135 @@
+var carta1 = {
+    nome: "Daniel Hartmann",
+    imagem:
+      "https://i.pinimg.com/564x/b0/0c/e6/b00ce60c04742ec1c63260740db3b541.jpg",
+    atributos: {
+      Sanidade: 7,
+      Força: 3,
+      Ocultismo: 7,
+      Inteligência: 6
+    }
+  };
+  
+  var carta2 = {
+    nome: "Thiago Fritz",
+    imagem:
+      "https://i.pinimg.com/236x/a1/95/af/a195af7a5723fa867fe7dd403fff312d.jpg",
+    atributos: {
+      Sanidade: 8,
+      Força: 4,
+      Ocultismo: 6,
+      Inteligência: 5
+    }
+  };
+  
+  var carta3 = {
+    nome: "Alexsander Kothe",
+    imagem:
+      "https://i.pinimg.com/564x/ff/a1/3a/ffa13a98bbabc6d21ca9440e61180371.jpg",
+    atributos: {
+      Sanidade: 5,
+      Força: 2,
+      Ocultismo: 5,
+      Inteligência: 7
+    }
+  };
+  
+  var carta4 = {
+    nome: "Cristopher Cohen",
+    imagem:
+      "https://i.pinimg.com/564x/6e/a7/8d/6ea78dc966c50b6d4e374d616e59dccd.jpg",
+    atributos: {
+      Sanidade: 5,
+      Força: 10,
+      Ocultismo: 3,
+      Inteligência: 6
+    }
+  };
+  
+  var carta5 = {
+    nome: "Arthur Cervero",
+    imagem:
+      "https://i.pinimg.com/564x/76/0c/e2/760ce2c6c9155fb722cf446fecc9e8a8.jpg",
+    atributos: {
+      Sanidade: 6,
+      Força: 6,
+      Ocultismo: 8,
+      Inteligência: 3
+    }
+  };
+  
+  var carta6 = {
+    nome: "Cristopher Cohen",
+    imagem:
+      "https://i.pinimg.com/564x/6e/a7/8d/6ea78dc966c50b6d4e374d616e59dccd.jpg",
+    atributos: {
+      Sanidade: 5,
+      Força: 10,
+      Ocultismo: 3,
+      Inteligência: 6
+    }
+  };
+  
+  var carta7 = {
+    nome: "Cesar Cohen (Kaiser)",
+    imagem:
+      "https://i.pinimg.com/564x/9a/1b/04/9a1b04e6822d0b01810e6a6119172acf.jpg",
+    atributos: {
+      Sanidade: 8,
+      Força: 2,
+      Ocultismo: 7,
+      Inteligência: 8
+    }
+  };
+  
+  var carta8 = {
+    nome: "Elizabeth Webber (Liz)",
+    imagem:
+      "https://i.pinimg.com/564x/60/1e/f9/601ef9c32f06cb13566686fb95b7e221.jpg",
+    atributos: {
+      Sanidade: 9,
+      Força: 7,
+      Ocultismo: 5,
+      Inteligência: 10
+    }
+  };
+  
+  var carta9 = {
+    nome: "Joui Jouki",
+    imagem:
+      "https://i.pinimg.com/564x/c0/71/2b/c0712b6a1d33b7ecf77487ded264fd0b.jpg",
+    atributos: {
+      Sanidade: 8,
+      Força: 7,
+      Ocultismo: 10,
+      Inteligência: 6
+    }
+  };
+  
+  var carta10 = {
+    nome: "Erin Parker",
+    imagem:
+      "https://i.pinimg.com/564x/0a/e9/8f/0ae98f05e647e06a3335ce72f95a755c.jpg",
+    atributos: {
+      Sanidade: 2,
+      Força: 4,
+      Ocultismo: 4,
+      Inteligência: 9
+    }
+  };
+  
+  var cartas = [
+    carta1,
+    carta2,
+    carta3,
+    carta4,
+    carta5,
+    carta6,
+    carta7,
+    carta8,
+    carta9,
+    carta10
+  ];
+  
+  var cartaMaquina;
+  var cartaJogador;
